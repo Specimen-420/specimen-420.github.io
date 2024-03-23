0868b5681363820c91e60f7b45199975bbef7556
Specimen <79764173+Specimen-420@users.noreply.github.com>
2024-03-23 14:27:41 +0000

    BOCK SELECT

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Get all the boxes
-var boxes = document.querySelectorAll('#twelve, #thirteen, #fourteen, #fifteen');
+var boxes = document.querySelectorAll('#one, #two, #three, #four, #five, #six, #seven, #eight, #nine, #ten, #eleven, #twelve, #thirteen, #fourteen, #fifteen, #sixteen, #seventeen, #eighteen');
 
 // Function to handle box click
 function handleBoxClick(event) {
